f9366222ac8dec1ce5e9f1102eaa8e4abefc756a
rakesh1423 <rakeshkumarpatra1423@gmail.com>
2026-08-19 16:11:53 +0530

    fix supabase environment variable

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabasePublishableKey =
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
-  supabasePublishableKey
+  supabaseAnonKey
 );
